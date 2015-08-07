0caf9eb3620a1848cbafd8eb711f20dc31275296
Hany Eldeib <hkeldeib@gmail.com>
2015-08-07 12:59:56 -0400

    Learning Project First Commit

---
+++ Documents/Hany/New folder/Learning/src/ApplesOranges.java
@@ -0,0 +1,17 @@
+
+public class ApplesOranges {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int numOranges = 5;
+		int numApples = 10;
+		int numFruits =0;
+		
+		numFruits = numOranges + numApples;
+		
+		// Show the results
+		System.out.println("Total Fruit Count is " + numFruits);
+
+	}
+
+}
