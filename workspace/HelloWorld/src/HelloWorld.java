2fb2b3aeb3e121ebb9351a9dc3d8e9e641b0c94e
Hany Eldeib <hkeldeib@gmail.com>
2015-08-14 18:47:05 -0400

    A version of Hello World that prints 4 lines.

--- workspace/HelloWorld/src/HelloWorld.java
+++ workspace/HelloWorld/src/HelloWorld.java
@@ -5,6 +5,8 @@ public class HelloWorld {
 		// Comment
 		System.out.println(" Hello World");
 		System.out.println(" Hello World");
+		System.out.println(" Hello World");
+		System.out.println(" Hello World");
 
 	}
 
