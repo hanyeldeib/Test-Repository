6ec6315c002229e28954f507724c08f67a88cebb
Hany Eldeib <hany.eldeib@campus.viu.edu>
2015-08-15 13:08:49 -0400

    Changed Hello 2 to Hello 3

--- Documents/Hany/New folder/Hello2/src/Hello2.java
+++ Documents/Hany/New folder/Hello2/src/Hello2.java
@@ -3,8 +3,9 @@ public class Hello2 {
 
 	public static void main(String[] args) {
 		// Comment
-		System.out.println("Hello 2");
-		System.out.println("Hello 2");
+		System.out.println("Hello 3");
+		System.out.println("Hello 3");
+		System.out.println("Hello 3");
 
 	}
 
