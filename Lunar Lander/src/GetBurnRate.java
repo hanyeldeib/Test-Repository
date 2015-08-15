f907fb7d58bd1789c694918fbc8860bc9242c063
Hany Eldeib <hany.eldeib@campus.viu.edu>
2015-08-15 15:56:05 -0400

    The Lunar Lander Model first piece

---
+++ Lunar Lander/src/GetBurnRate.java
@@ -0,0 +1,34 @@
+import java.io.*;
+
+public class GetBurnRate {
+	public static void main(String[] args){
+
+	System.out.println("Start Lunar Lander");
+	
+	try{
+		BufferedReader inputReader =
+				new BufferedReader(new BufferedReader(new InputStreamReader(System.in)));
+		
+		int burnRate = 0;
+		do {
+			System.out.println("Enter burn rate 0r <0 to quit: ");
+			
+			try{
+				String burnRateString = 
+						inputReader.readLine();
+				burnRate = 	Integer.parseInt(burnRateString);
+				
+				System.out.println("%" + burnRate);
+			}
+				Catch(NumberFormatException nfe)
+				{
+				System.out.println("#Invalid burn rate");
+				}
+				while(burnRate >=0);
+				inputReader.close();
+		}
+				catch(IOExeption ioe){
+					ioe.printStackTrace();
+			}
+		}
+	}
